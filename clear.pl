ad08f3f434a43078951e0ea8e77ff66c3f5926cf
Alfeo <floriandelabre1995@gmail.com>
2016-04-27 20:06:36 +0200

    fix space

--- clear.pl
+++ clear.pl
@@ -3,4 +3,4 @@ system("sudo rm -rf ./app/cache/* ./app/logs/*");
 system("sleep 0.1");
 print("\n\nAttribution des droits ...");
 system("sudo chmod 777 ./app/cache ./app/logs");
-print("\n\nTerminé");
+print("\n\nTerminé\n\n");
